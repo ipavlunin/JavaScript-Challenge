6b8b4e590b4bbc11b4fcebf20a1f30a2e042a570
ipavlunin <ipavlunin@gmail.com>
2020-11-05 17:33:32 -0600

    Table data passed to html

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,18 @@
-// from data.js
+// Data from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Table body variables
+var ufoTable = d3.select("#ufo-table");
+var ufoBody = ufoTable.select("tbody");
+
+// tableData iteration and calling function for each item
+tableData.forEach(ufo => {
+    // creating rows for data
+    var rowUfo = ufoBody.append('tr');
+    // array objects itteration to pick values
+    Object.entries(ufo).forEach(([key, value]) => {
+        // creating data cells and adding values
+        var entryUfo = rowUfo.append("td");
+        entryUfo.text(value);
+    });
+});
