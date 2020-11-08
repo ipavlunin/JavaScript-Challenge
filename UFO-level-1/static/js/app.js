c84bb216e05ddf0fc2afa16505cdcf8c937e7ae0
ipavlunin <ipavlunin@gmail.com>
2020-11-07 20:32:07 -0600

    step1 complete

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,3 +1,5 @@
+// Background sound
+
 // Data from data.js
 var tableData = data;
 
@@ -16,3 +18,30 @@ tableData.forEach(ufo => {
         entryUfo.text(value);
     });
 });
+
+// Creating listener
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+button.on("click", runSearch);
+form.on("submit", runSearch);
+
+// Filtering table
+function runSearch() {
+    // Preventing HTML events
+    d3.event.preventDefault();
+    // Filtering inputs
+    var inputType = d3.select("#datetime");
+    var inputVal = inputType.property("value");
+    var filterTable = tableData.filter(datapoint => datapoint.datetime === inputVal);
+    console.log(filterTable);
+    // Delete previous table data
+    ufoBody.selectAll("tr").remove();
+    // Looping in filtered data and update table
+    filterTable.forEach(function (ufo) {
+        var rowNew = ufoBody.append("tr");
+        Object.entries(ufo).forEach(function ([key, value]) {
+            var cellNew = rowNew.append("td");
+            cellNew.text(value);
+        });
+    });
+};
