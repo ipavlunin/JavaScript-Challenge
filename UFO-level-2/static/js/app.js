ea183676e1c5621957de9dd4d95414fabd3824bc
ipavlunin <ipavlunin@gmail.com>
2020-11-09 22:35:41 -0600

    tag ID code

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,3 +1,5 @@
+// Part1 - Data rendering and date search
+
 // Data from data.js
 var tableData = data;
 
@@ -44,8 +46,68 @@ function runSearch() {
     });
 };
 
-// Drop-Down filters
+// Part2 - Drop-down filters
 
-var selector = d3.select("#selFilter");
 // Listener on drop-down filters
+var selector = d3.select("#selFilter");
 selector.on("change", changeFilter);
+
+// SelectFilter function
+function selectFilter() {
+    var newFilter = selector.node().value;
+    var listInput = d3.select(".list-group-item");
+    var newInput = listInput.append('input');
+    var oldInput = d3.select("input");
+    var label = d3.select("label");
+    switch (newFilter) {
+        case "newFilter2":
+            oldInput.remove();
+            label.text("Enter City Name");
+            newInput.attr("class", "form-control");
+            newInput.attr("id", "city");
+            newInput.attr("type", "text");
+            newInput.attr("placeholder", "City");
+            break;
+        case "newFilter3":
+            oldInput.remove();
+            label.text("Enter State Name");
+            newInput.attr("class", "form-control");
+            newInput.attr("id", "state");
+            newInput.attr("type", "text");
+            newInput.attr("placeholder", "State");
+            break;
+        case "newFilter4":
+            oldInput.remove();
+            label.text("Enter Country Name");
+            newInput.attr("class", "form-control");
+            newInput.attr("id", "country");
+            newInput.attr("type", "text");
+            newInput.attr("placeholder", "Country");
+            break;
+        case "newFilter5":
+            oldInput.remove();
+            label.text("Enter Shape");
+            newInput.attr("class", "form-control");
+            newInput.attr("id", "shape");
+            newInput.attr("type", "text");
+            newInput.attr("placeholder", "Shape");
+            break;
+        default:
+            oldInput.remove();
+            label.text("Enter Date");
+            newInput.attr("class", "form-control");
+            newInput.attr("id", "datetime");
+            newInput.attr("type", "text");
+            newInput.attr("placeholder", "mm/dd/yyyy");
+            break;
+    }
+}
+
+// Input tag ID
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+// Listener for "Filter Table" button click action
+button.on("click", runSearch);
+// Listener for keyboard "ENTER"
+form.on("submit", runSearch);
+
