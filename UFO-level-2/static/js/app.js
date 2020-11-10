56162406978ac9731c777ee177bdf1f84a12a49f
ipavlunin <ipavlunin@gmail.com>
2020-11-09 23:10:11 -0600

    js update

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -6,6 +6,7 @@ var tableData = data;
 // Table body variables
 var ufoTable = d3.select("#ufo-table");
 var ufoBody = ufoTable.select("tbody");
+var oldInput = d3.select("input");
 
 // tableData iteration and calling function for each item
 tableData.forEach(ufo => {
@@ -27,17 +28,24 @@ form.on("submit", runSearch);
 
 // Filtering table
 function runSearch() {
+    var filterTab = [];
     // Preventing HTML events
     d3.event.preventDefault();
     // Filtering inputs
-    var inputType = d3.select("#datetime");
+    var inputType = d3.select(".form-control");
     var inputVal = inputType.property("value");
-    var filterTable = tableData.filter(datapoint => datapoint.datetime === inputVal);
-    console.log(filterTable);
+    if (filterTab.length >= 1) {
+        console.log('filtering filtered data');
+        filterTab = filterTab.filter(datapoint => datapoint.datetime === inputVal);
+    } else {
+        console.log('new filter');
+        filterTab = tableData.filter(datapoint => datapoint.datetime === inputVal);
+    }
+    console.log(filterTab);
     // Delete previous table data
     ufoBody.selectAll("tr").remove();
     // Looping in filtered data and update table
-    filterTable.forEach(function (ufo) {
+    filterTab.forEach(function (ufo) {
         var rowNew = ufoBody.append("tr");
         Object.entries(ufo).forEach(function ([key, value]) {
             var cellNew = rowNew.append("td");
@@ -50,7 +58,7 @@ function runSearch() {
 
 // Listener on drop-down filters
 var selector = d3.select("#selFilter");
-selector.on("change", changeFilter);
+selector.on("change", selectFilter);
 
 // SelectFilter function
 function selectFilter() {
@@ -111,3 +119,41 @@ button.on("click", runSearch);
 // Listener for keyboard "ENTER"
 form.on("submit", runSearch);
 
+// RunSearch function to filter by selected criteria
+var filterTab = [];
+function runSearch() {
+    var newId = d3.select("input").attr("id");
+    d3.event.preventDefault();
+    // Locating input tag by class
+    var inputType = d3.select(".form-control");
+    // Select entered input values
+    var inputVal = inputType.property("value");
+    var inputVal = inputVal.toLowerCase();
+    // Filtering
+    if (filterTab.length >= 1) {
+        console.log('filtering filtered data');
+        filterTab = filterTab.filter(datapoint => datapoint[newId] === inputVal);
+    } else {
+        console.log('new filter');
+        filterTab = tableData.filter(datapoint => datapoint[newId] === inputVal);
+    }
+    // Log out filtered results to console
+    console.log(filterTab);
+    // Delete previous table data
+    ufoBody.selectAll("tr").remove();
+    // Filter and update
+    filterTab.forEach(function (ufo) {
+        var rowNew = ufoBody.append("tr");
+        Object.entries(ufo).forEach(function ([key, value]) {
+            var cellNew = rowNew.append("td");
+            cellNew.text(value);
+        });
+    });
+};
+
+var resetButton = d3.select("#reset-btn");
+resetButton.on("click", resetFilters);
+
+function resetFilters() {
+    location.reload();
+}
